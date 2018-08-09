c5778d201ae4aa76ff91272e23cb9f36e9ae45dc
vagrant <vagrant@homestead>
2018-08-09 13:43:01 +0000

    Topic order

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -14,11 +14,13 @@ class TopicsController extends Controller
         $this->middleware('auth', ['except' => ['index', 'show']]);
     }
 
-	public function index()
+	public function index(Request $request, Topic $topic)
 	{
         //通过 Eloquent 提供的 预加载功能 方法 with() 提前加载了我们后面需要用到的关联属性 user 和 category，并做了缓存
-		$topics = Topic::with('user', 'category')->paginate(30);
-		return view('topics.index', compact('topics'));
+		//$topics = Topic::with('user', 'category')->paginate(30);
+
+        $topics = $topic->withOrder($request->order)->paginate(20);
+        return view('topics.index', compact('topics'));
 	}
 
     public function show(Topic $topic)
