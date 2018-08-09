5cbcf979343bdb99b600818ac3bdbe84b6ea0ab7
vagrant <vagrant@homestead>
2018-08-09 08:02:13 +0000

    Topic index page

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -16,7 +16,8 @@ class TopicsController extends Controller
 
 	public function index()
 	{
-		$topics = Topic::paginate();
+        //通过 Eloquent 提供的 预加载功能 方法 with() 提前加载了我们后面需要用到的关联属性 user 和 category，并做了缓存
+		$topics = Topic::with('user', 'category')->paginate(30);
 		return view('topics.index', compact('topics'));
 	}
 
