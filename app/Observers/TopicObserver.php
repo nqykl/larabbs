e41132128e562168adb06b57e8e6f310b7918dc5
vagrant <vagrant@homestead>
2018-08-11 06:25:18 +0000

    User can delete reply

--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -43,4 +43,13 @@ class TopicObserver
             dispatch(new TranslateSlug($topic));
         }
     }
+
+    /**
+     * 当话题被删除的时候，数据库里的回复信息没有存在的价值，只会占用空间。所以接下来我们将监听话题删除成功的事件，在此事件发生时，我们会删除此话题下所有的回复
+     * @param Topic $topic
+     */
+    public function deleted(Topic $topic)
+    {
+        \DB::table('replies')->where('topic_id', $topic->id)->delete();
+    }
 }
